b087711848de4331272516ac30aae923cb8c624c
Celso Henrique <celso.henrique@hotmail.com.br>
2021-03-24 22:22:15 -0300

    Removing useless methods

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import random
 from flask import jsonify,request,Flask
 import pandas as pd
+
 NarutoApi = Flask(__name__)
 df = pd.read_csv('finalQuotes.csv', names=['Speakers', 'Quotes'])
 
@@ -8,30 +9,12 @@ def get_random_quote():
     rq = random.randint(0, len(df))
     speaking = df.values[rq][0]
     quote = df.values[rq][1]
-    x = {"Speaker": speaking, "Quote": quote}
-    return x
+    quote_obj = {"Speaker": speaking, "Quote": quote}
+    return quote_obj
 
-def char_quotes(character):
-    df2 = df
-    df2 = df2[df2.Speakers.str.contains(character)]
-    print(df2)
-    rq = random.randint(0, len(df2))
-    speaking = df2.values[rq][0]
-    quote = df2.values[rq][1]
-    x = {"Speaker": speaking, "Quote": quote}
-    return x
-
-@NarutoApi.route('/NarutoQuotes/Random', methods=['GET'])
-def first_method():
-    x = get_random_quote()
-    return jsonify(x)
-
-@NarutoApi.route('/NarutoQuotes', methods=['GET'])
+@NarutoApi.route('/', methods=['GET'])
 def dashboard():
-    if request.method == 'GET':
-        shinobi = request.args.get('character', '')
-    x = char_quotes(shinobi)
-    return jsonify(x)
+    return jsonify(get_random_quote())
 
 if __name__ == '__main__':
     NarutoApi.run(host= '127.0.0.1',port=5000)
